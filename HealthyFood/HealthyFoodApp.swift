386890c011026115602619c3b784a84a157575f6
Li Yiu Yeung <rayli26go@gmail.com>
2025-01-23 03:05:04 +0800

    log out function

--- HealthyFood/HealthyFoodApp.swift
+++ HealthyFood/HealthyFoodApp.swift
@@ -10,23 +10,23 @@ import FirebaseCore
 import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
 }
 
 @main
 struct HealthyFoodApp: App {
     let persistenceController = PersistenceController.shared
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
+    @State private var isLoggedOut = false
+
     var body: some Scene {
         WindowGroup {
-            LoginView().environment(\.managedObjectContext, persistenceController.container.viewContext)
-            
+            LoginView(isLoggedOut: $isLoggedOut)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
