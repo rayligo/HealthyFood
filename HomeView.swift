3fe1940caa9685b732ff23f798e914a99203e953
Li Yiu Yeung <rayli26go@gmail.com>
2025-01-23 17:14:36 +0800

    add go to Upload Healthy Recipes function edit notebook Location

--- HomeView.swift
+++ HomeView.swift
@@ -5,14 +5,13 @@
 //  Created by Li Yiu Yeung  on 17/1/2025.
 //
 
-
 import SwiftUI
 import FirebaseAuth
 
 struct HomeView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
-    let disciplines = ["statue", "mural", "plaque"]
+    @StateObject private var HomedataViewModel = ViewModel()
+    
     @State private var isLoggedOut = false
     @State private var isNavigatingToLogin = false
 
@@ -28,26 +27,48 @@ struct HomeView: View {
                 TabView {
                     NavigationView {
                         VStack(spacing: 20) {
-                            List(disciplines, id: \.self) { discipline in
-                                NavigationLink(destination: DetailView(discipline: discipline)) {
-                                    Text(discipline)
+                            if HomedataViewModel.isLoading {
+                                ProgressView()
+                            } else {
+                                List(HomedataViewModel.list) { item in
+                                    NavigationLink(destination: RecipeDetailView(recipe: item)) {
+                                        HStack {
+                                            if let uiImage = item.imageFromBase64() {
+                                                Image(uiImage: uiImage)
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(height: 100)
+                                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                            }
+                                            VStack(alignment: .leading) {
+                                                Text(item.id + "." + item.Name)
+                                                    .font(.headline)
+                                                    .padding(.bottom, 2)
+                                                
+                                                
+                                            }
+                                            .padding()
+                                        }
+                                    }
                                 }
                             }
-
-                            Spacer()
+                        }
+                        .onAppear {
+                            HomedataViewModel.getData()
                         }
                         .toolbar {
-                            ToolbarItem(placement: .navigationBarLeading) {
-                                Text("HealthyFood")
-                                    .font(.largeTitle)
-                            }
-                            ToolbarItem(placement: .navigationBarTrailing) {
-                                NavigationLink(destination: NoteBookView().environment(\.managedObjectContext, viewContext)) {
-                                    Image("notebook")
-                                        .resizable()
-                                        .frame(width: 40, height: 40)
-                                        .background(Color.white)
-                                        .cornerRadius(5)
+                            ToolbarItem(placement: .principal) {
+                                HStack {
+                                    Text("Healthy Recipes")
+                                        .font(.largeTitle)
+                                    Spacer()
+                                    NavigationLink(destination: UploadHealthyRecipesView()) {
+                                        Image("addButton")
+                                            .resizable()
+                                            .frame(width: 40, height: 40)
+                                            .background(Color.white)
+                                            .cornerRadius(5)
+                                    }
                                 }
                             }
                         }
@@ -56,7 +77,7 @@ struct HomeView: View {
                         Image(systemName: "house.fill")
                         Text("Home")
                     }
-
+                    
                     NavigationView {
                         MapView()
                             .navigationBarTitle("Map")
@@ -66,7 +87,7 @@ struct HomeView: View {
                         Image(systemName: "map.fill")
                         Text("Map")
                     }
-
+                    
                     NavigationView {
                         AIFruitClassificationView()
                             .navigationBarTitle("Fruit Classification")
@@ -76,6 +97,17 @@ struct HomeView: View {
                         Image(systemName: "camera.fill")
                         Text("Classify")
                     }
+                    
+                    NavigationView {
+                        NoteBookView()
+                            .navigationBarTitle("Notebook")
+                            .navigationBarBackButtonHidden(true)
+                    }
+                    .tabItem {
+                        Image(systemName: "note.text")
+                        Text("Notebook")
+                    }
+                    
 
                     NavigationView {
                         LogOutView(isLoggedOut: $isLoggedOut)
