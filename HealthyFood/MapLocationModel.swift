2d75a96dcb503bc0379a3269d12d40b6c2e6002b
Li Yiu Yeung <rayli26go@gmail.com>
2025-01-28 16:08:07 +0800

    add comment

--- HealthyFood/MapLocationModel.swift
+++ HealthyFood/MapLocationModel.swift
@@ -7,10 +7,12 @@
 
 import CoreLocation
 import MapKit
-
+//Manage User Location
 class MapLocationModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    //// Update the corresponding map view
     @Published var coordinateRegion: MKCoordinateRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 22.2849, longitude: 114.1549), // Initial location
+        // Initial position
+        center: CLLocationCoordinate2D(latitude: 22.2849, longitude: 114.1549),
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
     @Published var currentLocationAnnotation: CurrentLocationAnnotation?
@@ -26,25 +28,25 @@ class MapLocationModel: NSObject, ObservableObject, CLLocationManagerDelegate {
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
-    
+    // Permission to access the user's location
     func requestPermission() {
         locationManager.requestWhenInUseAuthorization()
     }
-    
+    // Set the initial area of ​​the map
     func setupInitialRegion() {
         // Setup initial region
         if let location = locationManager.location {
             coordinateRegion.center = location.coordinate
         }
     }
-    
+    // Update location authorization status
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         self.authorizationStatus = status
         if status == .authorizedWhenInUse || status == .authorizedAlways {
             manager.startUpdatingLocation()
         }
     }
-    
+    // Update the current position when the position changes
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else { return }
         self.currentLocationAnnotation = CurrentLocationAnnotation(coordinate: location.coordinate)
@@ -53,7 +55,7 @@ class MapLocationModel: NSObject, ObservableObject, CLLocationManagerDelegate {
             self.coordinateRegion.center = location.coordinate
         }
     }
-    
+    // Set whether to update the map area
     func setShouldUpdateRegion(_ shouldUpdate: Bool) {
         self.shouldUpdateRegion = shouldUpdate
     }
